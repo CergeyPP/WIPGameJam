21feab22b61207d9904f09e65805043063803a5c
veryEasyBoy <soldersen@mail.ru>
2024-11-03 22:28:46 +0300

    Обновление кода

--- Assets/AssetWithTrap/Script/TaimingTrup.cs
+++ Assets/AssetWithTrap/Script/TaimingTrup.cs
@@ -8,26 +8,30 @@ internal class TaimingTrup : MonoBehaviour
 {
     private bool IsTaim = true;
     private bool IsTaim2 = true;
+    private bool IsTaim3 = true;
     private static float randomPlatforma;
+    private static float randomPlatforma2;
     public static string namePlatform;
     [SerializeField] private TMP_Text text;
     private static float Time = 1f;
     private void RandomTrapActive()
     {
-        if(Taimer.sec == 15 && IsTaim)
+        if(Taimer.sec >= 15 && IsTaim)
         {
             IsTaim = false;
             Debug.Log("Hard");
             StartCoroutine(TaimForTrap());
         }
-        else if (Taimer.sec == 30 && IsTaim2)
+        else if (Taimer.sec >= 30 && IsTaim2)
         {
             IsTaim2 = false;
             Debug.Log("Hard2");
             Time = 0.8f;
         }
-        else if (Taimer.sec == 40)
+        else if (Taimer.sec >= 45 && IsTaim3 )
         {
+            IsTaim3 = false;
+            Time = 0.5f;
             StartCoroutine(TaimForTrap2());
             Debug.Log(Time);
         }
@@ -49,11 +53,14 @@ internal class TaimingTrup : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(Time);
-            randomPlatforma = Random.Range(0, 4);
+            randomPlatforma2 = Random.Range(0, 4);
             yield return new WaitForSeconds(Time);
-            namePlatform = $"pan{randomPlatforma}";
-            Debug.Log(namePlatform);
-            yield return new WaitForSeconds(Time);
+            if (randomPlatforma2 != randomPlatforma)
+            {
+                namePlatform = $"pan{randomPlatforma2}";
+                Debug.Log(namePlatform + "Другая");
+                yield return new WaitForSeconds(Time);
+            }
         }
     }
     private void Update()
